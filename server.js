edd7aab91a4fadd57f1bc8233cddca8ad6135c6c
David Daly <dalyd14@gmail.com>
2021-01-23 15:49:13 -0500

    added read routes and a delete route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,80 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message)
+    }
+    console.log('Connected to the election database.')
+})
 
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows)
+// })
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        })
+    })    
+})
+
+app.get('/api/candidates/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    })    
+})
+
+app.delete('/api/candidates/:id', (req, res) => {
+    const sql = `DELETE FROM candidates 
+                 WHERE id = ?`;
+    const params = [req.params.id]
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        })
+    })    
+})
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.run(sql, params, function(err, result) {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID)
+// })
 
 app.use((req, res) => {
     res.status(404).end()
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`)
+    })    
 })

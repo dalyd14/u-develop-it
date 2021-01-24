f368bea94b3b45aa479b791d61356cb6dad76635
David Daly <dalyd14@gmail.com>
2021-01-23 19:03:25 -0500

    restructured routes; created voter routers

---
+++ db/database.js
@@ -0,0 +1,12 @@
+const { Module } = require('module');
+
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message)
+    }
+    console.log('Connected to the election database.')
+})
+
+module.exports = db;
